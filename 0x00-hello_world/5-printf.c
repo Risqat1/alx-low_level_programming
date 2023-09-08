9703c6e7a2794c202032c3389b4066a0e16a8bac
Risqat1 <Kolawolerisqat17@gmail.com>
2023-09-07 17:10:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main . Hello a program that  print f function 
+ * Return: 0
+ */
+
+int main(void)
+printf("with proper grammar, but the outcome is a piece of art,"\n)
+return (0);
+}
