a88e7113d163340c8e1ba085d03d9b2295732a23
Risqat1 <Kolawolerisqat17@gmail.com>
2023-10-19 08:07:15 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements
+ * @h: pointer to the list_t
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		a++;
+	}
+
+	return (a);
+}
